94c3d026fe4a0d857f645f0cd19f721e0ccbb321
hollowspy <julien.niedzwiecki@neuf.fr>
2022-03-31 11:52:36 +0200

    delete unused code

--- app.js
+++ app.js
@@ -23,12 +23,6 @@ app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-//   next();
-// })
 app.use(cors({origin: true, credentials: true}))
 
 // Use auth middleware
